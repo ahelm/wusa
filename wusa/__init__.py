0e4215562686f2ef8a65e9c16c1e2633a0c7bee9
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-12-22 21:46:43 +0000

    adds try/except if client is not running

--- wusa/__init__.py
+++ wusa/__init__.py
@@ -1,10 +1,19 @@
 from pathlib import Path
+import sys
 
+from click import secho
 import docker
 import appdirs
 from docker.models.configs import ConfigCollection
 
-CLIENT = docker.from_env()
+try:
+    CLIENT = docker.from_env()
+except docker.errors.DockerException as e:
+    secho("[ERROR] Can't obtain docker client!", fg="red", err=True)
+    secho("[ERROR] Is docker deamon running?", fg="red", err=True)
+    sys.exit(-1)
+
+
 WUSA_CONFIG_DIR = Path(appdirs.user_config_dir(appname="wusa"))
 WUSA_CONFIG_FILE = WUSA_CONFIG_DIR / "config.json"
 WUSA_SERVICE_NAME = "wusa.cli"
