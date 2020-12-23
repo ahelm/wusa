d2cacd2999fa8f0f4d72b82b1d5b78aad56ea80c
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-12-23 23:25:18 +0000

    removes previous config setup

--- wusa/__init__.py
+++ wusa/__init__.py
@@ -1,9 +1,6 @@
-from pathlib import Path
 import sys
 
 import docker
-import appdirs
-from docker.models.configs import ConfigCollection
 
 try:
     CLIENT = docker.from_env()
@@ -11,9 +8,3 @@ except docker.errors.DockerException as e:
     print("[ERROR] Can't obtain docker client!", file=sys.stderr)
     print("[ERROR] Is docker deamon running?", file=sys.stderr)
     sys.exit(-1)
-
-
-WUSA_CONFIG_DIR = Path(appdirs.user_config_dir(appname="wusa"))
-WUSA_CONFIG_FILE = WUSA_CONFIG_DIR / "config.json"
-WUSA_SERVICE_NAME = "wusa.cli"
-WUSA_USERNAME = "wusa-runner"
