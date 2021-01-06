f41805d30c30a9c62a461558d79826f6685f5840
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-06 19:36:47 +0000

    removes unused exception assignment

--- wusa/__init__.py
+++ wusa/__init__.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 import sys
 
 import docker
 
 try:
     CLIENT = docker.from_env()
-except docker.errors.DockerException as e:
+except docker.errors.DockerException:
     print("[ERROR] Can't obtain docker client!", file=sys.stderr)
     print("[ERROR] Is docker deamon running?", file=sys.stderr)
     sys.exit(-1)
