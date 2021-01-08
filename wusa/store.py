24d19c190f8224e9c2878d07ecfe57e560357958
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-08 18:46:46 +0000

    adds functions for interacting with runner file

---
+++ wusa/store.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import json
+from contextlib import contextmanager
+from typing import Dict
+
+from wusa import WUSA_RUNNER_FILE
+
+
+@contextmanager
+def open_runners_file() -> Dict[str, str]:
+    runner_dict = json.loads(WUSA_RUNNER_FILE.read_text())
+    yield runner_dict
+    WUSA_RUNNER_FILE.write_text(json.dumps(runner_dict))
+
+
+def read_runners_file() -> Dict[str, str]:
+    return json.loads(WUSA_RUNNER_FILE.read_text())
