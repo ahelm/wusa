b59a48a3ca3ec048637a9972b49d49a062f169c7
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-08 18:47:24 +0000

    adds basic config files

--- wusa/__init__.py
+++ wusa/__init__.py
@@ -9,3 +9,11 @@ except docker.errors.DockerException:
     print("[ERROR] Can't obtain docker client!", file=sys.stderr)
     print("[ERROR] Is docker deamon running?", file=sys.stderr)
     sys.exit(-1)
+
+import typer
+from pathlib import Path
+
+APP_NAME = "wusa"
+WUSA_BASE_DIR = Path(typer.get_app_dir(APP_NAME))
+WUSA_CONFIG_FILE = WUSA_BASE_DIR / "config.json"
+WUSA_RUNNER_FILE = WUSA_BASE_DIR / "runner.json"
