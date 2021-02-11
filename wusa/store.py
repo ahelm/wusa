2a10a999f43e128adc370b1471f833a7c4114f38
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-02-11 13:09:44 +0000

    fixes typing error in 'store.py'

--- wusa/store.py
+++ wusa/store.py
@@ -2,12 +2,13 @@
 import json
 from contextlib import contextmanager
 from typing import Dict
+from typing import Iterator
 
 from wusa import WUSA_RUNNER_FILE
 
 
 @contextmanager
-def open_runners_file() -> Dict[str, str]:
+def open_runners_file() -> Iterator[Dict[str, str]]:
     runner_dict = json.loads(WUSA_RUNNER_FILE.read_text())
     yield runner_dict
     WUSA_RUNNER_FILE.write_text(json.dumps(runner_dict))
