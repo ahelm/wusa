40f9ba838c6a278ba85e8d63605afd38ec3b1dee
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-02-22 10:22:49 +0000

    Remove docstrings from functions

--- wusa/utils.py
+++ wusa/utils.py
@@ -7,37 +7,10 @@ from wusa.store import read_runners_file
 
 
 def is_valid_url(url_to_check: str) -> bool:
-    """URL validator
-
-    Checks if a string is a valid url.
-
-    Parameters
-    ----------
-    url_to_check : str
-        String to check.
-
-    Returns
-    -------
-    bool
-        `True` if the passed string is a valid URL
-    """
     return url(url_to_check) is True
 
 
 def generate_container_name() -> str:
-    """Runner name generator
-
-    Creates a string which represents a valid name for a wusa runner. The string is a
-    combination of the string ``wusa_`` and an uuid of length 8. The runner name should
-    be unique, but name clashes can occur. To achieve uniqueness, the function tries to
-    generate up to 100 times an unique name and if it fails, the CLI is exited with an
-    error.
-
-    Returns
-    -------
-    str
-        String of a valid wusa runner name
-    """
     runners = read_runners_file()
 
     for _ in range(100):
